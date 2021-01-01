3387f6a105cccf404a32b2d01854e3e6ec6c53c2
Igor Krivenko <igor.s.krivenko@gmail.com>
2021-01-01 17:59:26 +0300

    Update MPL headers

--- docs/holstein.py
+++ docs/holstein.py
@@ -2,7 +2,7 @@
 # This file is part of pycommute, Python bindings for the libcommute C++
 # quantum operator algebra library.
 #
-# Copyright (C) 2020 Igor Krivenko <igor.s.krivenko@gmail.com>
+# Copyright (C) 2020-2021 Igor Krivenko <igor.s.krivenko@gmail.com>
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
